3d64dbd4e70f23a4d8d2554b6b7354393ab945d5
omidoyin <omidoyinayodeji@yahoo.com>
2026-03-26 19:23:35 +0100

    Fix: fix memory issues

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -34,7 +34,8 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding='utf-8',
-        case_sensitive=False
+        case_sensitive=False,
+        extra='ignore'
     )
 
 
